6f5f1fe980bad998c1cfed319239fadf551b2d20
Swathi Anil <117733383+sia-nyx@users.noreply.github.com>
2024-01-12 21:40:35 +0530

    Create Time server.cs

---
+++ Time server.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Time_Server
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024]; 
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//Berkeley sockets is an application programming interface (API) for Internet sockets and Unix domain sockets, used for inter-process communication (IPC). It is commonly implemented as a library of linkable modules.
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine();
+
+        }
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client Connected");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            
+                Socket socket = (Socket)AR.AsyncState;
+                int received = socket.EndReceive(AR);
+                byte[] dataBuf = new byte[received];
+                Array.Copy(_buffer, dataBuf, received);
+                string text = Encoding.ASCII.GetString(dataBuf);
+                Console.WriteLine("Text received: " + text);
+                string response = string.Empty;
+                if (text.ToLower() != "hello")
+                {
+                    response = "Invalid Request";
+                }
+                else
+                {
+                    response = DateTime.Now.ToLongTimeString();
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(response);
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                socket.BeginReceive(_buffer,0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+        }
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+
+
+        }
+    }
+}
+
+
+
+
+
+
